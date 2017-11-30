35f529b4407418e2648c7bcce7e18a7608ba9851
Amod Vaidya <amod.vaidya@yahoo.com>
2017-11-30 11:28:47 +0530

    syntax changes as suggested by VSCode

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -7,16 +7,16 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
-  name:string;
-  age:number;
-  email:string;
-  address:Address;
-  hobbies:string[];
-  hello:any;
-  posts:Post[];
-  isEdit:boolean = false;
+  name: string;
+  age: number;
+  email: string;
+  address: Address;
+  hobbies: string[];
+  hello: any;
+  posts: Post[];
+  isEdit: boolean = false;
 
-  constructor(private dataService:DataService) {
+  constructor(private dataService: DataService) {
     console.log('constructor ran..');
 
   }
@@ -24,54 +24,54 @@ export class UserComponent implements OnInit {
   ngOnInit() {
     console.log('ngOnInit ran...');
 
-    this.name = 'John Doe';
+    this.name = 'Amod Vaidya';
     this.email = 'test@test.com';
     this.age = 30;
     this.address = {
-      street:'50 Main st',
+      street: '50 Main st',
       city: 'Boston',
-      state:'MA'
+      state: 'MA'
     }
     this.hobbies = ['Write code', 'Watch movies', 'Listen to music'];
-    this.hello ='hello';
+    this.hello = 'hello';
 
     this.dataService.getPosts().subscribe((posts) => {
-      //console.log(posts);
+      // console.log(posts);
       this.posts = posts;
     });
   }
 
-  onClick(){
-    this.name='Brad Traversy';
+  onClick() {
+    this.name = 'Brad Traversy';
     this.hobbies.push('New Hobby');
   }
 
-  addHobby(hobby){
+  addHobby(hobby) {
     console.log(hobby);
     this.hobbies.unshift(hobby);
     return false;
   }
 
-  deleteHobby(i){
+  deleteHobby(i) {
     this.hobbies.splice(i, 1);
   }
 
-  toggleEdit(){
+  toggleEdit() {
     this.isEdit = !this.isEdit;
   }
 
 }
 
 
-interface Address{
-  street:string,
-  city:string,
-  state:string
+interface Address {
+  street: string,
+  city: string,
+  state: string
 }
 
-interface Post{
+interface Post {
   id: number,
-  title:string,
-  body:string,
-  userId:number
+  title: string,
+  body: string,
+  userId: number
 }
